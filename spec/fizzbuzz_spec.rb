9b86e070e6b026a6ac873e953dda8c7a5f893c68
Daisy <ysiad926@gmail.com>
2022-11-01 17:28:33 +0000

    finishing fizzbuzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,13 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "4" when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
